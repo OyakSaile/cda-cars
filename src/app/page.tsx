0c907b519bb844a22a1102994213139ee0ac1526
Kayo Elias <contatodevkayo@gmail.com>
2023-05-01 03:45:50 -0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>teste</h1>
+    </main>
+  );
+}
